611d9cf066b99f96d2293d4e5b3efef34a9e562e
Raphael Pinel <Raphael.Pinel@knowit.fi>
2024-05-28 00:41:45 +0300

    Create the frontend

---
+++ frontend/README.md
@@ -0,0 +1,9 @@
+# Microservices with Go
+
+## Frontend web server
+How to start the web server:
+- `cd frontend`
+- `go run cmd/frontend/main.go`
+- if prompted, allow network permissions
+- open in a browser `http://localhost`
+
---
+++ frontend/cmd/frontend/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+func main() {
+	
+	// Register the handler function
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		renderTemplate(w)
+	})
+	
+	// Start the server
+	log.Println("Server started on: http://localhost:80")
+	err := http.ListenAndServe(":80", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func renderTemplate(w http.ResponseWriter) {
+	log.Println("Rendering template...")
+
+	partials := []string{
+		// content.html should be the first file
+		"./cmd/frontend/templates/content.html",
+		"./cmd/frontend/templates/layout.html",
+		"./cmd/frontend/templates/head.html",
+	}
+
+	tmpl, err := template.ParseFiles(partials...)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// execute the template and return Internal Server Error if it fails
+	if err := tmpl.Execute(w, nil); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+
